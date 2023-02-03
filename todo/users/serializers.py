cad09574bffc41ff0e1743dac022b42c38f9a987
Peka97 <ivan.karasyov97@gmail.com>
2023-02-04 02:14:20 +0300

    Tasks complete

---
+++ todo/users/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import CustomUser
+
+
+class CustomUserModelSerializer(ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        # fields = ('username', 'first_name', 'last_name', 'email')
