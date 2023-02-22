dc6ab7ab0a2532777d2800ce1ffb29f7a4bfc17b
Peka97 <ivan.karasyov97@gmail.com>
2023-02-22 12:36:25 +0000

    Lesson 5

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
 
 // import logo from './logo.svg';
 import './App.css';
 import MenuList from './components/Menu.js';
 import UserList from './components/User.js';
+import ProjectsList from './components/Projects';
+import ToDoList from './components/TODO';
 import FooterList from './components/Footer.js';
+import NotFound404 from './components/PageNotFound404';
 
 
 class App extends React.Component {
@@ -14,26 +18,13 @@ class App extends React.Component {
         this.state = {
             'menu': [],
             'users': [],
+            'projects': [],
+            'todos': [],
             'footer': [],
         }
     }
 
     componentDidMount() {
-
-        // Данные из переменной
-        // const users = [
-        //     {
-        //         'first_name': 'Иван',
-        //         'last_name': 'Карасёв',
-        //         'email': 'vk@mail.ru'
-        //     },
-        //     {
-        //         'first_name': 'Кирилл',
-        //         'last_name': 'Захаренко',
-        //         'email': 'fdsatrew@mail.ru'
-        //     },
-        // ]
-
         const menu = [{
             'main': 'Главное',
             'contacts': 'Контакты',
@@ -46,34 +37,62 @@ class App extends React.Component {
 
         this.setState(
             {
-                // 'users': users,
                 'menu': menu,
                 'footer': footer,
             }
         )
 
 
-        // Данные с сервера (не подгружаются, пишет ошибку)
-        axios.get('http://127.0.0.1:8000/api/users', { headers: { 'Access-Control-Allow-Origin': '*' } })
-            .then(response => {
-                const users = response.data
-                this.setState(
-                    {
-                        'users': users,
-                    }
-                )
-            }).catch(error => console.log(error))
+        axios.get('http://127.0.0.1:8000/api/users/', {
+            headers: { 'Access-Control-Allow-Origin': 'http://127.0.0.1:3000' }
+        }).then(response => {
+            this.setState({ 'users': response.data.results })
+        }).catch(error => console.log(error))
+
+
+        axios.get('http://127.0.0.1:8000/api/projects/', {
+            headers: { 'Access-Control-Allow-Origin': 'http://127.0.0.1:3000' }
+        }).then(response => {
+            this.setState({ 'projects': response.data.results })
+        }).catch(error => console.log(error))
+
+
+        axios.get('http://127.0.0.1:8000/api/TODO/', {
+            headers: { 'Access-Control-Allow-Origin': 'http://127.0.0.1:3000' }
+        }).then(response => {
+            this.setState({ 'todos': response.data.results })
+        }).catch(error => console.log(error))
     }
+
 
     render() {
         return (
-            <div>
-                <div><MenuList menu={this.state.menu} /></div>
-                <hr></hr>
-                <div><UserList users={this.state.users} /></div>
-                <hr></hr>
-                <div><FooterList footers={this.state.footer} /></div>
-            </div >
+            <div className='App'>
+                <BrowserRouter>
+                    <nav ><MenuList menu={this.state.menu} /></nav>
+                    <nav>
+                        <ul >
+                            <li>
+                                <Link to='/' >Users</Link>
+                            </li>
+                            <li>
+                                <Link to='/projects' >Projects</Link>
+                            </li>
+                            <li>
+                                <Link to='/TODO' >TODO</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route exact path="/" component={() => <UserList users={this.state.users} />} />
+                        <Route exact path="/projects" component={() => <ProjectsList projects={this.state.projects} />} />
+                        <Redirect from="/users" to="/"></Redirect>
+                        <Route exact path="/TODO" component={() => <ToDoList todos={this.state.todos} />} />
+                        <Route component={NotFound404} />
+                    </Switch>
+                    <nav><FooterList footers={this.state.footer} /></nav>
+                </BrowserRouter>
+            </div>
         )
     }
 }
